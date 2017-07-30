990eea409bbc39c53771924f42a7b44cb5d405a6
Adrian <adriandinca2008@yahoo.com>
2017-07-30 16:40:26 +0300

    reduced the size of the script

--- js/sidenav.js
+++ js/sidenav.js
@@ -5,25 +5,11 @@
     let overlayRest = document.getElementById('overlay-rest');
     let html = document.getElementsByTagName('html')[0];
 
-    menuBtn.addEventListener('click', OpenSidenav);
+    menuBtn.addEventListener('click', ToggleSidenav);
+    overlayRest.addEventListener('click', ToggleSidenav);
 
-    function OpenSidenav() {
-        // disable scroll
-        document.body.classList.add('no-scroll');
-        html.classList.add('no-scroll');
-
-        // display the sidenav
-        overlay.classList.remove('hide');
-    }
-
-    overlayRest.addEventListener('click', CloseSidenav);
-
-    function CloseSidenav() {
-        // enable scroll
-        document.body.classList.remove('no-scroll');
-        html.classList.remove('no-scroll');
-
-        // hide the sidenav
-        overlay.classList.add('hide');
+    function ToggleSidenav() {
+        document.body.classList.toggle('no-scroll');
+        html.classList.toggle('no-scroll');
     }
 })();
