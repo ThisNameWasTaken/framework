804206fba1e4aa33059d4b4bb53a7a668e3ab7e7
Adrian <adriandinca2008@yahoo.com>
2017-08-09 14:07:57 +0300

    the ripple effect works properly on mobile

--- js/ripples.js
+++ js/ripples.js
@@ -1,88 +1,96 @@
-(function () {
-    // get all the elements with a 'ripple-effect'
-    let elements = document.getElementsByClassName('ripple-effect');
 
-    for (let i = 0; i < elements.length; i++) {
-        AttachRipple(elements[i]);
+// get all the elements with a 'ripple-effect'
+let elements = document.getElementsByClassName('ripple-effect');
 
-        elements[i].addEventListener('mousedown', DisplayRipple);
-        elements[i].addEventListener('mouseup', FadeRipple);
+for (let i = 0; i < elements.length; i++) {
+    SetupRippleEffect(elements[i]);
 
-        elements[i].addEventListener('touchstart', DisplayRipple);
-        elements[i].addEventListener('touchend', FadeRipple);
+    elements[i].addEventListener('mousedown', DisplayRipple);
+    elements[i].addEventListener('mouseup', FadeRipple);
+    elements[i].addEventListener('mouseleave', FadeRipple);
+
+    elements[i].addEventListener('touchstart', DisplayRipple);
+    elements[i].addEventListener('touchend', FadeRipple);
+}
+
+let ignoreMouseDown = ignoreMouseUp = false;
+
+function DisplayRipple() {
+
+    if (event.type === 'mousedown') {
+        if (ignoreMouseDown) {
+            ignoreMouseDown = false;
+            return;
+        }
+    } else if (event.type === 'touchstart') {
+        ignoreMouseDown = ignoreMouseUp = true;
     }
 
-    let ignoreMouse = false;
+    let rippleContainer = this.getElementsByClassName('ripple-container')[0];
+    let ripple = rippleContainer.lastElementChild;
+    ripple.classList.add('is-visible');
 
-    function DisplayRipple() {
-        if (event.type === 'mousedown') {
-            if (ignoreMouse) {
-                ignoreMouse = false;
-                return;
-            }
-        } else if (event.type === 'touchstart') {
-            ignoreMouse = true;
+    // Display the ripple 
+    SetRipplePos(event, this);
+}
+
+function FadeRipple() {
+    if (event.type === 'mouseup')
+        if (ignoreMouseUp) {
+            ignoreMouseUp = false;
+            return;
         }
 
-        this.classList.remove('is-fading');
-        SetRipplePos(event, this);
-        let self = this;
-        setTimeout(function () { self.classList.add('is-visible'); }, 0);
-    }
+    let rippleContainer = this.getElementsByClassName('ripple-container')[0];
+    let ripple = rippleContainer.lastElementChild;
 
-    function FadeRipple() {
-        this.classList.remove('is-visible');
-        this.classList.add('is-fading');
-    }
+    if (!ripple.classList.contains('is-visible'))
+        return;
 
-    function AttachRipple(element) {
-        let rippleContainer = document.createElement('div');
-        rippleContainer.classList.add('ripple-container');
+    ripple.classList.remove('is-visible');
+    // Fade the ripple 
+    ripple.classList.add('is-fading');
 
-        let ripple = document.createElement('span');
-        ripple.classList.add('ripple');
+    setTimeout(function () { rippleContainer.removeChild(ripple); }, 600);
 
-        AddRippleShade(ripple, element);
+    // Create a new ripple
+    AddRippleToElement(this);
+}
 
-        ripple.style.width = ripple.style.height = Math.max(element.clientHeight, element.clientWidth) + 'px';
+function SetupRippleEffect(element) {
+    let rippleContainer = document.createElement('div');
+    rippleContainer.classList.add('ripple-container');
 
-        rippleContainer.appendChild(ripple);
-        element.appendChild(rippleContainer);
-    }
+    element.appendChild(rippleContainer);
 
-    function SetRipplePos(event, element) {
-        if (element.classList.contains('check'))
-            return;
+    AddRippleToElement(element);
+}
 
-        // get element offset
-        let offset = element.getBoundingClientRect();
-        // get click coords
-        let x = event.clientX !== undefined ? event.clientX : event.touches[0].clientX;
-        let y = event.clientY !== undefined ? event.clientY : event.touches[0].clientY;
+function AddRippleToElement(element) {
+    let ripple = document.createElement('span');
+    ripple.classList.add('ripple');
 
-        x = (x - offset.left) + 'px';
-        y = (y - offset.top) + 'px';
+    ripple.style.width = ripple.style.height = Math.max(element.clientHeight, element.clientWidth) + 'px';
 
-        let ripple = element.getElementsByClassName('ripple')[0];
-        ripple.style.top = y;
-        ripple.style.left = x;
-    }
+    let rippleContainer = element.getElementsByClassName('ripple-container')[0];
+    rippleContainer.appendChild(ripple);
+}
 
-    function AddRippleShade(ripple, element) {
-        if (element.classList.contains('check'))
-            return;
+function SetRipplePos(event, element) {
+    if (element.classList.contains('check'))
+        return;
 
-        // we choose ripple's color based on element's background color
-        let bgColor = window.getComputedStyle(element).backgroundColor;
-        // get each color value
-        let rgba = bgColor.replace(/^rgba?\(|\s+|\)$/g, '').split(',');
+    // get element offset
+    let offset = element.getBoundingClientRect();
+    // get click coords
+    let x = event.clientX !== undefined ? event.clientX : event.touches[0].clientX;
+    let y = event.clientY !== undefined ? event.clientY : event.touches[0].clientY;
 
-        // if the color is whitish add a black ripple
-        if (rgba[0] >= 210 && rgba[1] >= 210 && rgba[2] >= 210)
-            ripple.classList.add('black');
-        else if (rgba[3] <= 0.35)
-            ripple.classList.add('black');
-        else // otherwhise add a white ripple
-            ripple.classList.add('white');
-    }
-})();
+    x = (x - offset.left) + 'px';
+    y = (y - offset.top) + 'px';
+
+    let rippleContainer = element.getElementsByClassName('ripple-container')[0];
+    let ripple = rippleContainer.lastElementChild;
+    ripple.style.top = y;
+    ripple.style.left = x;
+}
