17aa131912686a784df825833ce45f4abd18d207
Adrian <adriandinca2008@yahoo.com>
2017-08-17 11:17:22 +0300

    * z-indexes updated accordingly to the material design guidelines
    * tabs and buttons are now completely independent

--- js/tabs.js
+++ js/tabs.js
@@ -1,26 +1,16 @@
 (function () {
     'use strict';
 
-    let tabGroups = document.getElementsByClassName('tab__group');
+    let tabBars = document.getElementsByClassName('tab__bar');
 
-    for (let i = 0; i < tabGroups.length; i++) {
-        let tabItems = tabGroups[i].getElementsByClassName('tab__item');
+    for (let i = 0; i < tabBars.length; i++) {
+        let tabItems = tabBars[i].getElementsByClassName('tab__item');
         let lastActiveTab = undefined;
 
         for (let i = 0; i < tabItems.length; i++) {
-            let tabToggle = document.getElementById(tabItems[i].getAttribute('for'));
-            let tabContent = tabToggle.nextElementSibling;
-            // add the bottom bar
-            if (tabContent) {
-                let tabBar = document.createElement('span');
-                tabBar.classList.add('tab__bar');
-                tabBar.classList.add('primary');
-                tabItems[i].appendChild(tabBar);
-            }
-
-            // when a tab item is clicked add a bar to the bottom
             tabItems[i].addEventListener('click', SetTabActive);
-            // add the bottom-bar to the active element of the group
+
+            let tabToggle = document.getElementById(tabItems[i].getAttribute('for'));
             if (tabToggle.checked)
                 tabItems[i].click();
         }
